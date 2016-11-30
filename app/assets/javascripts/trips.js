3a66269c93f0711cd60adb95f0dfe87c1aa32ced
Sebe Keller <sebethkeller@gmail.com>
2016-11-30 15:45:53 -0500

    Add autocomplete for locations on trip form using google places api

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load', function(){
+	console.log("Trips");
+	$('#location').keypress(function(){
+		var input = /** @type {!HTMLInputElement} */(
+            document.getElementById('location'));
+		console.log(input);
+		var autocomplete = new google.maps.places.Autocomplete(input);
+	});
+});
